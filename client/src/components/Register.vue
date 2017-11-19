834a9d830f9ec71b0542ebab11adde9b83414610
Ben Bousquet <hamfoftw@gmail.com>
2017-11-18 18:54:46 -0700

    added vueify and updated form

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,21 +1,49 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input
-      type="email"
-      name="email"
-      v-model="email"
-      placeholder="email" />
-    <br>
-    <input
-      type="password"
-      name="password"
-      v-model="password"
-      placeholder="password" />
-    <br>
-    <button
-      @click="register">Register</button>
-  </div>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Registration</v-toolbar-title>
+        </v-toolbar>
+        <v-form v-model="valid" ref="form" lazy-validation>
+          <v-text-field
+            label="Username"
+            v-model="username"
+            :counter="10"
+            required
+          ></v-text-field>
+          <v-text-field
+            label="E-mail"
+            v-model="email"
+            hint="Please enter a valid email address"
+            required
+          ></v-text-field>
+          <v-text-field
+            label="Password"
+            v-model="password"
+            hint="Password must be least 8 characters"
+            :append-icon="e1 ? 'visibility' : 'visibility_off'"
+            :append-icon-cb="() => (e1 = !e1)"
+            :type="e1 ? 'password' : 'text'"
+            required
+          ></v-text-field>
+          <v-checkbox
+            label="I agree to the Terms of Service and Privacy Policy"
+            v-model="checkbox"
+            :rules="[v => !!v || 'You must agree to sign up!']"
+            required
+          ></v-checkbox>
+          <div class="error" v-html="error"></div>
+          <v-btn
+            @click="register"
+            :disabled="!valid"
+          >
+            Register
+          </v-btn>
+        </v-form>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -23,22 +51,33 @@ import authService from '@/services/authService'
 export default {
   data () {
     return {
+      e1: false,
+      valid: true,
+      checkbox: false,
       email: '',
-      password: ''
+      username: '',
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-        const response = await authService.register({
-          email: this.email,
-          password: this.password
-        })
-        console.log(response.data)
+        try {
+          await authService.register({
+            email: this.email,
+            username: this.username,
+            password: this.password
+          })
+        } catch (error) {
+          this.error = error.response.data.error
+        }
      }
   }
 }
 </script>
 
 <style scoped>
-
+  .error {
+    color:red;
+  }
 </style>
